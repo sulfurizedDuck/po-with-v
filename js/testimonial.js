a18cf8a4de5a7436d6064abf5366c4402c3f5242
sulfurizedDuck <bebekbelerang3@gmail.com>
2019-06-18 10:33:59 +0700

    Changed testimonial

--- js/testimonial.js
+++ js/testimonial.js
@@ -24,7 +24,80 @@ $('#testimonialForm').on('submit', (e) => {
     e.preventDefault()
     alert('Review must be at least 7 words.')
   }
+  else if (isNaN($('#rating').val())) {
+    e.preventDefault()
+    alert('Rating must be numeric.')
+  }
+  else if ($('#rating').val() < 1 || $('#rating').val() > 5) {
+    e.preventDefault()
+    alert('Rating must be between 1 and 5.')
+  }
   else {
     alert('Thank you!')
   }
+})
+
+var inputTitle = $('input[id=title]')
+var inputRating = $('input[id=rating]')
+var inputAddress = $('input[id=address]')
+var inputReview = $('input[id=review]')
+
+// Name
+$('input[id=name]').on('focusout', function() {
+  inputVal = $('input[id=name]').val()
+  if (inputVal == '') 
+    $('label[for=name]').html('Name')
+  else
+    $('label[for=name]').html(inputVal) 
+})
+$('input[id=name]').on('focusin', function() {
+  $('label[for=name]').html('Name')
+})
+
+// Title
+$('input[id=title]').on('focusout', function() {
+  inputVal = $('input[id=title]').val()
+  if (inputVal == '') 
+    $('label[for=title]').html('Title')
+  else
+    $('label[for=title]').html(inputVal) 
+})
+$('input[id=title]').on('focusin', function() {
+  $('label[for=title]').html('Title')
+})
+
+// Rating
+$('input[id=rating]').on('focusout', function() {
+  inputVal = $('input[id=rating]').val()
+  if (inputVal == '') 
+    $('label[for=rating]').html('Rating')
+  else
+    $('label[for=rating]').html(inputVal) 
+})
+$('input[id=rating]').on('focusin', function() {
+  $('label[for=rating]').html('Rating')
+})
+
+// Address
+$('textarea[id=address]').on('focusout', function() {
+  inputVal = $('textarea[id=address]').val()
+  if (inputVal == '') 
+    $('label[for=address]').html('Address')
+  else
+    $('label[for=address]').html(inputVal) 
+})
+$('input[id=address]').on('focusin', function() {
+  $('label[for=address]').html('Address')
+})
+
+// Review
+$('textarea[id=review]').on('focusout', function() {
+  inputVal = $('textarea[id=review]').val()
+  if (inputVal == '') 
+    $('label[for=review]').html('Review')
+  else
+    $('label[for=review]').html(inputVal) 
+})
+$('input[id=review]').on('focusin', function() {
+  $('label[for=review]').html('Review')
 })
